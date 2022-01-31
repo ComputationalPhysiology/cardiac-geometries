5c4975e526412f0976e55af6b5b08535372e40e2
Henrik Finsberg <henriknf@simula.no>
2022-01-31 10:44:37 +0100

    Add calculus modulue

--- src/cardiac_geometries/__init__.py
+++ src/cardiac_geometries/__init__.py
@@ -1,17 +1,11 @@
-from .import_checks import has_dolfin
-from .import_checks import has_gmsh
-from .import_checks import has_ldrb
-from .import_checks import has_meshio
-from .import_checks import has_mshr
+from . import calculus
+from ._import_checks import has_dolfin
+from ._import_checks import has_gmsh
+from ._import_checks import has_ldrb
+from ._import_checks import has_meshio
+from ._import_checks import has_mshr
 
-
-__all__ = [
-    "has_dolfin",
-    "has_gmsh",
-    "has_ldrb",
-    "has_meshio",
-    "has_mshr",
-]
+__all__ = ["has_dolfin", "has_gmsh", "has_ldrb", "has_meshio", "has_mshr", "calculus"]
 
 if has_gmsh():
     from . import _gmsh as gmsh  # noqa: F401
