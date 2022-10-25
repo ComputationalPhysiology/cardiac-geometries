52c90cd17dfb83fbdf87d14383be1a32edf3b999
Henrik Finsberg <henrikfinsberg@hotmail.com>
2022-10-25 09:27:55 +0200

    Add test for cli-create-lv-ellipsoid

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -16,3 +16,19 @@ def test_create_slab(tmp_path: Path):
     assert outfile.is_file()
     geo = Geometry.from_file(outfile)
     assert geo.f0 is not None
+
+
+def test_create_lv_ellipsoid(tmp_path: Path):
+
+    runner = CliRunner()
+    res1 = runner.invoke(
+        cli.create_lv_ellipsoid,
+        ["--create-fibers", tmp_path.as_posix()],
+    )
+    assert res1.exit_code == 0
+    outfile = tmp_path / "geo.h5"
+    res2 = runner.invoke(cli.folder2h5, [tmp_path.as_posix(), "--outfile", outfile])
+    assert res2.exit_code == 0
+    assert outfile.is_file()
+    geo = Geometry.from_file(outfile)
+    assert geo.f0 is not None
