c2f911c7f5263c1e7e9966c8ddc9742cd3652fbe
Henrik Finsberg <henrikfinsberg@hotmail.com>
2024-04-02 18:35:30 +0200

    Add code for generating mesh for land 2015 benchmark

--- src/cardiac_geometries/_mesh.py
+++ src/cardiac_geometries/_mesh.py
@@ -626,6 +626,41 @@ def create_lv_ellipsoid(
     return geo
 
 
+def create_benchmark_geometry_land15(
+    r_short_endo: float = 7.0,
+    r_short_epi: float = 10.0,
+    r_long_endo: float = 17.0,
+    r_long_epi: float = 20.0,
+    quota_base: float = -5.0,
+    psize: float = 3.0,
+    ndiv: float = 1.0,
+    axisymmetric: bool = False,
+    fiber_space: str = "Quadrature_4",
+    fiber_angle_endo: float = -90,
+    fiber_angle_epi: float = +90,
+):
+    mu_base_endo = math.acos(quota_base / r_long_endo)
+    mu_base_epi = math.acos(quota_base / r_long_epi)
+    mu_apex_endo = mu_apex_epi = 0
+    psize_ref = psize / ndiv
+    return create_lv_ellipsoid(
+        r_short_endo=r_short_endo,
+        r_short_epi=r_short_epi,
+        r_long_endo=r_long_endo,
+        r_long_epi=r_long_epi,
+        mu_base_endo=mu_base_endo,
+        mu_base_epi=mu_base_epi,
+        mu_apex_endo=mu_apex_endo,
+        mu_apex_epi=mu_apex_epi,
+        psize_ref=psize_ref,
+        create_fibers=True,
+        fiber_angle_endo=fiber_angle_endo,
+        fiber_angle_epi=fiber_angle_epi,
+        fiber_space=fiber_space,
+        axisymmetric=axisymmetric,
+    )
+
+
 def create_slab(
     outdir: Optional[Union[Path, str]] = None,
     lx: float = 20.0,
